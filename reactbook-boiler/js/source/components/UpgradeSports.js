c4b3abc29a5875cb707b619f08dea223152ca8e0
David <davidhy_17@hotmail.com>
2018-02-05 23:08:34 +0000

    testing

---
+++ reactbook-boiler/js/source/components/UpgradeSports.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class UpgradeSport extends React.Component {
+
+    render() {
+        return
+        <div className='ignore'>
+            <img src="http://www.donkeyheaven.org/uploads/images/Gallery/donkeys/clive.jpg" alt="Smiley face" width="42" height="42">
+            </img>
+            <span>
+                asdf
+                </span>
+        </div>
+    };
+}
+
+export default UpgradeSport
